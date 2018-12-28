b2eabb8f418125acb5506289a1e3fda7fa39ee9a
PYRRH4 <PYRRH4@users.noreply.github.com>
2018-12-28 14:34:17 +0100

    PyrSlotMachine v2.0.1-BETA

--- src/be/pyrrh4/pyrslotmachine/PSMPerm.java
+++ src/be/pyrrh4/pyrslotmachine/PSMPerm.java
@@ -4,7 +4,7 @@ import be.pyrrh4.pyrcore.lib.Perm;
 
 public class PSMPerm {
 
-	public static final Perm PYRSLOTMACHINE_ROOT = new Perm("PYRSLOTMACHINE_ROOT", null, "pyrslotmachine");
-	public static final Perm PYRSLOTMACHINE_ADMIN = new Perm("PYRSLOTMACHINE_ADMIN", PYRSLOTMACHINE_ROOT, "admin");
+	public static final Perm PYRSLOTMACHINE_ROOT = new Perm(null, "pyrslotmachine.*");
+	public static final Perm PYRSLOTMACHINE_ADMIN = new Perm(PYRSLOTMACHINE_ROOT, "pyrslotmachine.admin");
 
 }
